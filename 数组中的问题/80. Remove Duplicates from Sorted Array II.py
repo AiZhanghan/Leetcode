79dfcd6d757e605997a48b6b28820b031a1856ce
Liferry <35103759+Liferry@users.noreply.github.com>
2019-05-26 20:00:02 +0800

    1

--- 数组中的问题/80. Remove Duplicates from Sorted Array II.py
+++ 数组中的问题/80. Remove Duplicates from Sorted Array II.py
@@ -1,20 +1,32 @@
 class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
+    def removeDuplicates(self, nums) -> int:
         #[i, j)内为重复元素
         i = 0
         #被删除元素个数:j - i - 1
         remove_num= 0
         #数组长度
         lenth = len(nums)
-        while i + 1 < len(nums) - remove_num:
+        while i + 2 < len(nums) - remove_num:
             
             j = i + 1
-            while nums[j] == nums[i]:
+            while j < len(nums) - remove_num and nums[j] == nums[i]:
                 j += 1
             
+            #没有重复
             if j - i <= 2:
                 i = j
                 continue
             else:
-                remove_num += j - i - 2
-                
+                redundance_num = j - i - 2
+                nums[i + 2: lenth - remove_num - redundance_num] = nums[j: lenth - remove_num]
+                remove_num += redundance_num
+                i += 2
+        
+        return lenth - remove_num
+    
+if __name__ == '__main__':
+    arr = [0,1,2,2,2,2,2,3,4,4,4]
+    
+    res = Solution().removeDuplicates(nums=arr)
+    
+    print(arr, res)
