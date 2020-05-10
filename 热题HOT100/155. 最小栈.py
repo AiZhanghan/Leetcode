c95df1e0bf4dd54c28a24f759b456d7d3e5406d4
黄振凯 <35103759+AiZhanghan@users.noreply.github.com>
2020-05-10 10:13:30 +0800

    top 100

---
+++ 热题HOT100/155. 最小栈.py
@@ -0,0 +1,36 @@
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.data = []
+        self.helper = []
+
+    def push(self, x: int) -> None:
+        self.data.append(x)
+        if not self.helper or x <= self.helper[-1]:
+            self.helper.append(x)
+
+    def pop(self) -> None:
+        if self.data:
+            res = self.data.pop()
+            if self.helper[-1] == res:
+                self.helper.pop()
+            return res
+
+    def top(self) -> int:
+        if self.data:
+            return self.data[-1]
+
+    def getMin(self) -> int:
+        if self.helper:
+            return self.helper[-1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
