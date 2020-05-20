72b9225afd94301f16b1117021b95b4432d7df0a
黄振凯 <35103759+AiZhanghan@users.noreply.github.com>
2020-05-20 09:50:17 +0800

    top100

--- 热题HOT100/287. 寻找重复数.py
+++ 热题HOT100/287. 寻找重复数.py
@@ -1,5 +1,5 @@
 class Solution:
-    def findDuplicate(self, nums: List[int]) -> int:
+    def findDuplicate(self, nums):
         """
         Args:
             nums: list[int]
@@ -7,4 +7,48 @@ class Solution:
         Return: 
             int
         """
+        fast = 0
+        slow = 0
         
+        while True:
+            fast = nums[nums[fast]]
+            slow = nums[slow]
+            if fast == slow:
+                break
+        
+        finder = 0
+        while True:
+            finder = nums[finder]
+            slow = nums[slow]
+            if finder == slow:
+                break
+        
+        return slow
+
+
+# class Solution:
+#     def findDuplicate(self, nums):
+#         """
+#         Args:
+#             nums: list[int]
+        
+#         Return: 
+#             int
+#         """
+#         left = 0
+#         right = len(nums) - 1
+
+#         while left < right:
+#             mid = left + (right - left) // 2
+            
+#             count = 0
+#             for num in nums:
+#                 if num <= mid:
+#                     count += 1
+            
+#             if count > mid:
+#                 right = mid
+#             else:
+#                 left = mid + 1
+        
+#         return left
