e9f1a6898b5e610de910cc04e2d546cfcbf8f1de
黄振凯 <35103759+AiZhanghan@users.noreply.github.com>
2020-07-04 22:28:22 +0800

    1

--- code/106. 从中序与后序遍历序列构造二叉树.py
+++ code/106. 从中序与后序遍历序列构造二叉树.py
@@ -18,6 +18,20 @@ class Solution:
         """
         self.postorder = postorder
         self.dic = {val: i for i, val in enumerate(inorder)}
-        return self._build_tree()
+        return self._build_tree(0, len(inorder), 0, len(postorder))
 
-    def _build_tree(self, )
+    def _build_tree(self, in_left, in_right, post_left, post_right):
+        if in_left >= in_right:
+            return
+        
+        root = TreeNode(self.postorder[post_right - 1])
+        root_index = self.dic[root.val]
+        
+        left_len = root_index - in_left
+
+        root.left  = self._build_tree(in_left, root_index, 
+            post_left, post_left + left_len)
+        root.right = self._build_tree(root_index + 1, in_right,
+            post_left + left_len, post_right - 1)
+        
+        return root
