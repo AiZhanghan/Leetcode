131077d2df6005dc60d4841027d7a640f51cfe43
黄振凯 <35103759+AiZhanghan@users.noreply.github.com>
2020-08-09 15:47:48 +0800

    1

---
+++ code/324. 摆动排序 II.py
@@ -0,0 +1,9 @@
+class Solution:
+    def wiggleSort(self, nums):
+        """
+        Do not return anything, modify nums in-place instead.
+        
+        Args:
+            nums: list[int]
+        """
+        
