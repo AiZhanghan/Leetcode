b14a4dce561bd1495b8de3def37e515aaaa02efd
黄振凯 <35103759+AiZhanghan@users.noreply.github.com>
2020-08-20 21:51:38 +0800

    1

---
+++ code/650. 只有两个键的键盘.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minSteps(self, n):
+        """
+        Args:
+            n: int
+        
+        Return:
+            int
+        """
+        res = 0
+        d = 2
+        while n > 1:
+            while n % d == 0:
+                res += d
+                n //= d
+            d += 1
+        return res
