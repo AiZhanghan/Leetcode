6af00aae1ade3ccf3c760cb31c7f7ff205d9ee1f
黄振凯 <35103759+AiZhanghan@users.noreply.github.com>
2020-08-24 17:04:54 +0800

    1

--- code/565. 数组嵌套.py
+++ code/565. 数组嵌套.py
@@ -7,4 +7,14 @@ class Solution:
         Return:
             int
         """
-        
+        res = 0
+        for i in range(len(nums)):
+            count = 0
+            j = i
+            while nums[j] >= 0:
+                count += 1
+                temp = nums[j]
+                nums[j] = -1
+                j = temp
+            res = max(res, count)
+        return res
