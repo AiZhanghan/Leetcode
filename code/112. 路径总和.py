dfe8f8c6df85f9848d10e253996d9b98d4ba0f31
黄振凯 <35103759+AiZhanghan@users.noreply.github.com>
2020-08-30 15:40:45 +0800

    1

--- code/112. 路径总和.py
+++ code/112. 路径总和.py
@@ -16,4 +16,44 @@ class Solution:
         Return:
             bool
         """
+        if not root:
+            return False
         
+        if not root.left and not root.right:
+            return root.val == sum
+        
+        return self.hasPathSum(root.left, sum - root.val) or \
+            self.hasPathSum(root.right, sum - root.val)
+
+
+# class Solution:
+#     def hasPathSum(self, root, sum):
+#         """
+#         Args:
+#             root: TreeNode
+#             sum: int
+        
+#         Return:
+#             bool
+#         """
+#         if not root:
+#             return False
+#         self.res = False
+#         self.dfs(root, sum)
+#         return self.res
+    
+#     def dfs(self, root, sum):
+#         """
+#         Args:
+#             root: TreeNode
+#             sum = int
+#         """
+#         if self.res:
+#             return
+#         if not root.left and not root.right:
+#             self.res = (sum == root.val)
+#             return
+#         if root.left:
+#             self.dfs(root.left, sum - root.val)
+#         if root.right:
+#                 self.dfs(root.right, sum - root.val)
