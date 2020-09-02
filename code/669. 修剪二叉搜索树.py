03acd3ad5da4df7046e99b4e7ba0c3e606df9860
黄振凯 <35103759+AiZhanghan@users.noreply.github.com>
2020-09-02 21:05:41 +0800

    1

---
+++ code/669. 修剪二叉搜索树.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def trimBST(self, root, L, R):
+        """
+        Args:
+            root: TreeNode
+            L: int
+            R: int
+        
+        Return:
+            TreeNode
+        """
+        if not root:
+            return
+        if root.val > R:
+            return self.trimBST(root.left, L, R)
+        if root.val < L:
+            return self.trimBST(root.right, L, R)
+        
+        root.left = self.trimBST(root.left, L, R)
+        root.right = self.trimBST(root.right, L, R)
+        return root
