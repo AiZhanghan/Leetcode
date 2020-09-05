a5b7eedca37caa9d59d68ba167e389ddd98a877e
黄振凯 <35103759+AiZhanghan@users.noreply.github.com>
2020-09-05 18:17:34 +0800

    1

--- code/684. 冗余连接.py
+++ code/684. 冗余连接.py
@@ -1,3 +1,44 @@
+class UnionFind:
+    def __init__(self, n):
+        """
+        Args:
+            n: int
+        """
+        self.roots = [x for x in range(n + 1)]
+
+    def find(self, i):
+        """
+        Args:
+            i: int
+        
+        Return:
+            int
+        """
+        if self.roots[i] == i:
+            return self.roots[i]
+        self.roots[i] = self.find(self.roots[i])
+        return self.roots[i]
+    
+    def is_connected(self, p, q):
+        """
+        Args:
+            p: int
+            q: int
+        
+        Return:
+            bool
+        """
+        return self.find(p) == self.find(q)
+    
+    def union(self, p, q):
+        """
+        Args:
+            p: int
+            q: int
+        """
+        self.roots[self.find(p)] = self.find(q)
+
+
 class Solution:
     def findRedundantConnection(self, edges):
         """
@@ -7,4 +48,8 @@ class Solution:
         Return:
             list[int]
         """
-        
+        uf = UnionFind(len(edges))
+        for p, q in edges:
+            if uf.is_connected(p, q):
+                return [p, q]
+            uf.union(p, q)
