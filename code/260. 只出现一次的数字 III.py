5a5400bfe4e2f13175078b4791bb716950226f06
黄振凯 <35103759+AiZhanghan@users.noreply.github.com>
2020-09-06 09:44:15 +0800

    1

---
+++ code/260. 只出现一次的数字 III.py
@@ -0,0 +1,24 @@
+class Solution:
+    def singleNumber(self, nums):
+        """
+        Args:
+            nums: list[int]
+        
+        Return:
+            list[int]
+        """
+        xor = 0
+        for num in nums:
+            xor ^= num
+        
+        mask = xor & (-xor)
+        
+        a = 0
+        b = 0
+        for num in nums:
+            if num & mask:
+                a ^= num
+            else:
+                b ^= num
+        
+        return [a, b]
