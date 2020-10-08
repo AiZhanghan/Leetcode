c4ff4736a6c45db32ebcd3c9d7dfa1caf3b3d713
黄振凯 <35103759+AiZhanghan@users.noreply.github.com>
2020-10-08 09:28:53 +0800

    1

--- code/1. 两数之和.py
+++ code/1. 两数之和.py
@@ -1,28 +1,16 @@
-from typing import List
-
-
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum(self, nums, target):
+        """
+        Args:
+            nums: list[int]
+            target: int
+        
+        Return:
+            list[int]
+        """
         dic = {}
         for i, num in enumerate(nums):
             complement = target - num
             if complement in dic:
                 return [dic[complement], i]
             dic[num] = i
-
-# class Solution:
-#     def twoSum(self, nums: List[int], target: int) -> List[int]:
-#         index = [x for x in range(len(nums))]
-#         index.sort(key=lambda x: nums[x])
-#         left = 0
-#         right = len(nums) - 1
-#         res = []
-#         while left < right:
-#             if nums[index[left]] + nums[index[right]] == target:
-#                 res = [index[left], index[right]]
-#                 break
-#             elif nums[index[left]] + nums[index[right]] < target:
-#                 left += 1
-#             else:
-#                 right -= 1
-#         return res
